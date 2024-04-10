dece9678d170156cebd3b1654ed8a007092a4c47
Kevin <kevin.322.vr@gmail.com>
2024-04-10 15:45:58 -0500

    fix: added vite plugin to fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import './App.css';
-
 function App() {
   return <div className="lato">reto rimac</div>;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import Unfonts from 'unplugin-fonts/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    Unfonts({
+      custom: {
+        families: [
+          {
+            name: 'BRSomona',
+            local: 'BRSomona',
+            src: '/public/assets/fonts/BRSonoma/*.otf',
+          },
+          {
+            name: 'Lato',
+            local: 'Lato',
+            src: '/public/assets/fonts/Lato/*.ttf',
+          },
+        ],
+      },
+    }),
+  ],
+});
